50f56f0881b8137c45e8a609f06fb4f6175d1dac
Vyacheslav <slawa.121@yandex.ru>
2026-07-04 23:24:11 +0300

    ДЗ 1. Часть 1

--- src/laba1/Runner.java
+++ src/laba1/Runner.java
@@ -2,11 +2,7 @@ package laba1;
 
 public class Runner {
     public static void main(String[] args) {
-
-        System.out.printf("Hello and welcome laba 1!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        MatrixValue[][] net = Solution.matrixNet();
+        Printer.printNetMatrix(net);
     }
 }
