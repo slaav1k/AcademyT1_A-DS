435c9c880d99ec897fd18169d85a4f9c6a70a95a
Vyacheslav <slawa.121@yandex.ru>
2026-07-05 07:50:26 +0300

    ДЗ 1. Часть 1 (Правки)

--- src/laba1/Runner.java
+++ src/laba1/Runner.java
@@ -2,7 +2,6 @@ package laba1;
 
 public class Runner {
     public static void main(String[] args) {
-        MatrixValue[][] net = Solution.matrixNet();
-        Printer.printNetMatrix(net);
+        System.out.println(Solution.matrixNet());
     }
 }
--- src/laba1/Solution.java
+++ src/laba1/Solution.java
@@ -2,33 +2,25 @@ package laba1;
 
 
 public class Solution {
-    public static MatrixValue[][] matrixNet() {
-        MatrixValue[][] net = new MatrixValue[6][6];
+    public static NetMatrix matrixNet() {
+        NetMatrix net = new NetMatrix(6);
 
         // A - B
-        net[0][1] = new MatrixValue(1500, 90);
-        net[1][0] = net[0][1];
+        net.setElement(0, 1, new MatrixValue(1500, 90));
         // A - C
-        net[0][2] = new MatrixValue(2000, 10);
-        net[2][0] = net[0][2];
+        net.setElement(0, 2, new MatrixValue(2000, 10));
         // A - D
-        net[0][3] = new MatrixValue(1000, 50);
-        net[3][0] = net[0][3];
+        net.setElement(0, 3, new MatrixValue(1000, 50));
         // B - F
-        net[1][5] = new MatrixValue(1500, 60);
-        net[5][1] = net[1][5];
+        net.setElement(1, 5, new MatrixValue(1500, 60));
         // C - E
-        net[2][4] = new MatrixValue(900, 5);
-        net[4][2] = net[2][4];
+        net.setElement(2, 4, new MatrixValue(900, 5));
         // C - F
-        net[2][5] = new MatrixValue(500, 20);
-        net[5][2] = net[2][5];
+        net.setElement(2, 5, new MatrixValue(500, 20));
         // D - E
-        net[3][4] = new MatrixValue(2500, 1);
-        net[4][3] = net[3][4];
+        net.setElement(3, 4, new MatrixValue(2500, 1));
         // E - F
-        net[4][5] = new MatrixValue(300, 85);
-        net[5][4] = net[4][5];
+        net.setElement(4, 5, new MatrixValue(300, 85));
 
         return net;
     }
