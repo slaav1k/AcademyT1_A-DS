4f356929f47add7cc97ea50da3fe895ce277674a
Vyacheslav <slawa.121@yandex.ru>
2026-07-11 09:30:38 +0300

    ДЗ 2. Причесал код

--- src/laba2/Solution.java
+++ src/laba2/Solution.java
@@ -5,44 +5,37 @@ import java.util.List;
 public class Solution {
     public static Result findMinMax(List<Integer> pLst) {
         int n = pLst.size();
-        int startPos;
-        int min;
-        int max;
 
-        int tmpFirst = pLst.get(0);
+        int startPos = (n % 2 != 0) ? 1 : 2;
 
-        if (n % 2 != 0) {
-            min = tmpFirst;
-            max = tmpFirst;
-            startPos = 1;
-        } else {
-            int tmpSecond = pLst.get(1);
-            if (tmpFirst > tmpSecond) {
-                max = tmpFirst;
-                min = tmpSecond;
-            } else {
-                max = tmpSecond;
-                min = tmpFirst;
-            }
-            startPos = 2;
-        }
+        Result tmp = initMinMax(pLst, n);
+        int min = tmp.min();
+        int max = tmp.max();
 
-        int tmpMin, tmpMax, tmp;
+
+        int tmpMin, tmpMax;
 
         for (int i = startPos; i < n - 1; i += 2) {
-            tmpMin = pLst.get(i);
-            tmpMax = pLst.get(i + 1);
-
-            if (tmpMax < tmpMin) {
-                tmp = tmpMax;
-                tmpMax = tmpMin;
-                tmpMin = tmp;
-            }
+            tmp = getSortedPair(pLst.get(i), pLst.get(i + 1));
+            tmpMax = tmp.max();
+            tmpMin = tmp.min();
 
             if (min > tmpMin) min = tmpMin;
             if (max < tmpMax) max = tmpMax;
         }
 
         return new Result(min, max);
+    }
+
+    private static Result initMinMax(List<Integer> pLst, int pN) {
+        int tmpFirst = pLst.get(0);
+        if (pN % 2 != 0) return new Result(tmpFirst, tmpFirst);
+        return getSortedPair(tmpFirst, pLst.get(1));
+
+    }
+
+    private static Result getSortedPair(int pA, int pB) {
+        if (pA > pB) return new Result(pB, pA);
+        return new Result(pA, pB);
     }
 }
